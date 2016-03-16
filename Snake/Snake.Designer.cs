8b8769d4bee172723f47d2850760b9bc33d6d124
valekkp <valekkp@rambler.ru>
2016-03-16 23:34:58 +0300

    Recommiting

---
+++ Snake/Snake.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(7, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UpButton
+            // 
+            this.UpButton.Location = new System.Drawing.Point(458, 72);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(67, 36);
+            this.UpButton.TabIndex = 2;
+            this.UpButton.Text = "Up";
+            this.UpButton.UseVisualStyleBackColor = true;
+            this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.Location = new System.Drawing.Point(397, 85);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(55, 52);
+            this.LeftButton.TabIndex = 3;
+            this.LeftButton.Text = "Left";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
+            // 
+            // DownButton
+            // 
+            this.DownButton.Location = new System.Drawing.Point(458, 114);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(67, 32);
+            this.DownButton.TabIndex = 4;
+            this.DownButton.Text = "Down";
+            this.DownButton.UseVisualStyleBackColor = true;
+            this.DownButton.Click += new System.EventHandler(this.DownButton_Click);
+            // 
+            // RightButton
+            // 
+            this.RightButton.Location = new System.Drawing.Point(531, 85);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(59, 52);
+            this.RightButton.TabIndex = 5;
+            this.RightButton.Text = "Right";
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 361);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.LeftButton);
+            this.Controls.Add(this.UpButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Button RightButton;
+
+    }
+}
+
---
+++ Snake/Snake.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        const int n = 10;
+        enum State { head, tail, body, food, empty };
+        int[,] board = new int[n, n];
+        int[] index1 = new int[n*n];
+        int[] index2 = new int[n*n];
+        int SnakeLength = 1;
+        Random random = new Random();
+        int foodindex1, foodindex2;
+        int x = 20, y = 20, size = 30;
+        int dx = 0, dy = 0;
+        string direction = "Up";
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Size = new Size(40 + n * size, 40 + n * size);
+            ClientSize = new Size(300 + n * size, 40 + n * size);
+            
+
+            //Brush MyBrush = Brushes.Red;
+            //SolidBrush FillBrush = new SolidBrush(Color.Black);
+            //e.Graphics.FillRectangle(FillBrush, x, y, size, size);
+            //FillBrush = new SolidBrush(Color.Red);
+            //e.Graphics.FillRectangle(MyBrush, x + size*5 + 1, y + size*5 + 1, size - 1, size - 1);
+            //e.Graphics.FillRectangle(MyBrush, x + size * 6 + 1, y + size * 5 + 1, size - 1, size - 1);
+
+
+            //Змейка
+            board[5, 5] = 1;
+            for (int i = 0; i < n*n; i++)
+            {
+                index1[i] = -1;
+                index2[i] = -1;
+            }
+            index1[0] = 5;
+            index2[0] = 5;
+            foodindex1 = random.Next(0, 10);
+            foodindex2 = random.Next(0, 10);
+            //MyBrush = Brushes.Blue;
+            //e.Graphics.FillRectangle(MyBrush, x + size * 5 + 1, y + size * 5 + 1, size - 1, size - 1);
+            //Moving("Up", 1);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //Рисование линий
+            Pen MyPen = Pens.Black;
+            Brush MyBrush = Brushes.Blue;
+            Brush FoodBrush = Brushes.Red;
+            dx = dy = 0;
+            for (int i = 0; i < n + 1; i++)
+            {
+                e.Graphics.DrawLine(MyPen, x, y + dy, x + n * size, y + dy);
+                e.Graphics.DrawLine(MyPen, x + dx, y, x + dx, y + n * size);
+                dx += size;
+                dy += size;
+            }
+
+            for (int i = 0; i < SnakeLength; i++)
+            {
+                if((index1[i] != -1) && (index2[i] != -1))
+                    e.Graphics.FillRectangle(MyBrush, x + size * index1[i] + 1, y + size * index2[i] + 1, size - 1, size - 1);
+            }
+
+            if ((index1[0] == foodindex1) && (index2[0] == foodindex2))
+            {
+                SnakeLength++;
+                foodindex1 = random.Next(0, 10);
+                foodindex2 = random.Next(0, 10);
+            }
+            //Рисование еды
+            e.Graphics.FillRectangle(FoodBrush, x + size * foodindex1 + 1, y + size * foodindex2 + 1, size - 1, size - 1);
+                 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Moving(direction);
+            pictureBox1.Refresh();
+        }
+
+        private void InitializeTimer()
+        {
+            timer1.Interval = 500;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+            button1.Text = "Stop";
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        private void Moving(string direction) //Перемещение
+        {
+            if (direction == "Up")
+            {
+                for (int i = SnakeLength; i > 0; i--)
+                {
+                    if((index2[i] == index2[0] - 1) && (index1[i] == index1[0]))
+                    {
+                        timer1.Enabled = false;
+                        MessageBox.Show("You lost! =( Your Score: " + SnakeLength);
+                        return;
+                    }
+                    index1[i] = index1[i - 1];
+                    index2[i] = index2[i - 1];
+                }
+                if (index2[0] - 1 < 0)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("You lost! =( Your Score: " + SnakeLength);
+                    return;
+                }
+                index2[0]--;
+                index1[SnakeLength] = -1;
+                index2[SnakeLength] = -1;
+            }
+            if (direction == "Down")
+            {
+                for (int i = SnakeLength; i > 0; i--)
+                {
+                    if ((index2[i] == index2[0] + 1) && (index1[i] == index1[0]))
+                    {
+                        timer1.Enabled = false;
+                        MessageBox.Show("You lost! =( Your Score: " + SnakeLength);
+                        return;
+                    }
+                    index1[i] = index1[i - 1];
+                    index2[i] = index2[i - 1];
+                }
+                if (index2[0] + 1 > 9)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("You lost! =( Your Score: " + SnakeLength);
+                    return;
+                }
+                index2[0]++;
+                index1[SnakeLength] = -1;
+                index2[SnakeLength] = -1;
+            }
+            if (direction == "Left")
+            {
+                for (int i = SnakeLength; i > 0; i--)
+                {
+                    if ((index2[i] == index2[0]) && (index1[i] == index1[0] - 1))
+                    {
+                        timer1.Enabled = false;
+                        MessageBox.Show("You lost! =( Your Score: " + SnakeLength);
+                        return;
+                    }
+                    index1[i] = index1[i - 1];
+                    index2[i] = index2[i - 1];
+                }
+                if (index1[0] - 1 < 0)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("You lost! =( Your Score: " + SnakeLength);
+                    return;
+                }
+                    index1[0]--;
+                    index1[SnakeLength] = -1;
+                    index2[SnakeLength] = -1;
+            }
+            if (direction == "Right")
+            {
+                for (int i = SnakeLength; i > 0; i--)
+                {
+                    if ((index2[i] == index2[0]) && (index1[i] == index1[0] + 1))
+                    {
+                        timer1.Enabled = false;
+                        MessageBox.Show("You lost! =( Your Score: " + SnakeLength);
+                        return;
+                    }
+                    index1[i] = index1[i - 1];
+                    index2[i] = index2[i - 1];
+                }
+                if (index1[0] + 1 > 9)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("You lost! =( Your Score: " + SnakeLength);
+                    return;
+                }
+                    index1[0]++;
+                    index1[SnakeLength] = -1;
+                    index2[SnakeLength] = -1;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Stop")
+            {
+                button1.Text = "Start";
+                timer1.Enabled = false;
+            }
+            else
+            {
+                button1.Text = "Stop";
+                timer1.Enabled = true;
+            }
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            direction = "Up";
+        }
+
+        private void DownButton_Click(object sender, EventArgs e)
+        {
+            direction = "Down";
+        }
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            direction = "Left";
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
+            direction = "Right";
+        }
+    }
+}
