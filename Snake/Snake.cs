e10c6e2db665e0805640ad1eff28b6835c01ac80
valekkp <valekkp@rambler.ru>
2016-03-20 20:07:34 +0300

    Fixed bug, now snake can't go opposite direction when SnakeLength > 1

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -7,9 +7,14 @@ namespace Snake
     public partial class Form1 : Form
     {
         const int N = 10;
-        int[,] board = new int[N, N];
-        int[] indexX = new int[N * N];
-        int[] indexY = new int[N * N];
+        enum CellType {empty, body, food};
+        CellType [,] board = new CellType[N, N];
+        struct Point
+        {
+            public int X;
+            public int Y;
+        }
+        Point[] SnakeSegments = new Point[N * N];
         int SnakeLength = 1;
         int offsetX;
         int offsetY;
@@ -18,7 +23,7 @@ namespace Snake
         int foodIndexX, foodIndexY;
         int x = 0, y = 0, size = 30;
         int dx = 0, dy = 0;
-        string direction = "Right", previousDirection;
+        string direction = "Right";
         //bool test = false;
 
         public Form1()
@@ -31,41 +36,35 @@ namespace Snake
             //Змейка
             for (int i = 0; i < N * N; i++)
             {
-                indexX[i] = -1;
-                indexY[i] = -1;
+                SnakeSegments[i].X = -1;
+                SnakeSegments[i].Y = -1;
             }
-            indexX[0] = N / 2;
-            indexY[0] = N / 2;
-            board[indexY[0], indexX[0]] = 1;
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    board[i, j] = CellType.empty;
+            SnakeSegments[0].X = N / 2;
+            SnakeSegments[0].Y = N / 2;
+            board[SnakeSegments[0].Y, SnakeSegments[0].X] = CellType.body;
             //Еда
             do
             {
                 foodIndexX = random.Next(0, N);
                 foodIndexY = random.Next(0, N);
-            } while (board[foodIndexY, foodIndexX] == 1);
-            board[foodIndexY, foodIndexX] = 2;
+            } while (board[foodIndexY, foodIndexX] == CellType.body);
+            board[foodIndexY, foodIndexX] = CellType.food;
         }
 
         
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            //Рисование линий
             Pen linePen = Pens.Black;
             Brush SnakeBodyBrush = Brushes.DarkViolet;
             Brush FoodBrush = Brushes.Red;
             Brush SnakeHeadBrush = Brushes.Blue;
             dx = dy = 0;
-
-            /*for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    Console.Write(board[i, j]);
-                }
-                Console.WriteLine();
-            }*/
-
+            
+            //Рисование линий
             for (int i = 0; i < N + 1; i++)
             {
                 e.Graphics.DrawLine(linePen, x, y + dy, x + N * size, y + dy);
@@ -74,14 +73,11 @@ namespace Snake
                 dy += size;
             }
 
-            e.Graphics.FillRectangle(SnakeHeadBrush, x + size * indexX[0] + 1, y + size * indexY[0] + 1, size - 1, size - 1);
-
+            //Рисование змейки
+            e.Graphics.FillRectangle(SnakeHeadBrush, x + size * SnakeSegments[0].X + 1, y + size * SnakeSegments[0].Y + 1, size - 1, size - 1);
             for (int i = 1; i < SnakeLength; i++)
-            {
-                if ((indexX[i] != -1) && (indexY[i] != -1))
-                    e.Graphics.FillRectangle(SnakeBodyBrush, x + size * indexX[i] + 1, y + size * indexY[i] + 1, size - 1, size - 1);
-            }
-
+                if ((SnakeSegments[i].X != -1) && (SnakeSegments[i].Y != -1))
+                    e.Graphics.FillRectangle(SnakeBodyBrush, x + size * SnakeSegments[i].X + 1, y + size * SnakeSegments[i].Y + 1, size - 1, size - 1);
 
             //Рисование еды
             e.Graphics.FillRectangle(FoodBrush, x + size * foodIndexX + 1, y + size * foodIndexY + 1, size - 1, size - 1);
@@ -91,23 +87,23 @@ namespace Snake
         private void InitializeTimer()
         {
             timer1.Interval = 500;
-            //timer1.Enabled = true;
             timer1.Tick += timer1_Tick;
 
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            //previousDirection = direction;
             Moving(direction);
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             timer1.Enabled = true;
-            if (e.KeyCode == Keys.Up) { previousDirection = direction; direction = "Up"; }
-            if (e.KeyCode == Keys.Down) { previousDirection = direction; direction = "Down"; }
-            if (e.KeyCode == Keys.Left) { previousDirection = direction; direction = "Left"; }
-            if (e.KeyCode == Keys.Right) { previousDirection = direction; direction = "Right"; }
+            if (e.KeyCode == Keys.Up) { direction = "Up"; }
+            if (e.KeyCode == Keys.Down) { direction = "Down"; }
+            if (e.KeyCode == Keys.Left) {  direction = "Left"; }
+            if (e.KeyCode == Keys.Right) { direction = "Right"; }
         }
 
         private void Restart()
@@ -116,19 +112,15 @@ namespace Snake
             timer1.Interval = 500;
             for (int i = 0; i < SnakeLength; i++)
             {
-                indexX[i] = 0;
-                indexY[i] = 0;
+                SnakeSegments[i].X = 0;
+                SnakeSegments[i].Y = 0;
             }
             for (int i = 0; i < N; i++)
-            {
                 for (int j = 0; j < N; j++)
-                {
-                    board[i, j] = 0;
-                }
-            }
-            indexX[0] = N / 2;
-            indexY[0] = N / 2;
-            board[indexY[0], indexX[0]] = 1;
+                    board[i, j] = CellType.empty;
+            SnakeSegments[0].X = N / 2;
+            SnakeSegments[0].Y = N / 2;
+            board[SnakeSegments[0].Y, SnakeSegments[0].X] = CellType.body;
             SnakeLength = 1;
             pictureBox1.Refresh();
         }
@@ -146,29 +138,20 @@ namespace Snake
                 offsetX = -1;
             if (direction == "Right")
                 offsetX = 1;
-            if ((indexX[0] + offsetX == indexX[1]) && (indexY[0] + offsetY == indexY[1]))
+            if ((SnakeSegments[0].X + offsetX == SnakeSegments[1].X) && (SnakeSegments[0].Y + offsetY == SnakeSegments[1].Y))
             {
-                direction = previousDirection;
-                if (direction == "Up")
-                    offsetY = -1;
-                if (direction == "Down")
-                    offsetY = 1;
-                if (direction == "Left")
-                    offsetX = -1;
-                if (direction == "Right")
-                    offsetX = 1;
+                offsetX = -offsetX;
+                offsetY = -offsetY;
             }
 
-            Console.WriteLine(direction + " " + previousDirection);
-
-            int newHeadPositionX = indexX[0] + offsetX;
-            int newHeadPositionY = indexY[0] + offsetY;
+            int newHeadPositionX = SnakeSegments[0].X + offsetX;
+            int newHeadPositionY = SnakeSegments[0].Y + offsetY;
 
             if ((newHeadPositionY < 0) ||
                 (newHeadPositionX < 0) ||
                 (newHeadPositionY > N - 1) ||
                 (newHeadPositionX > N - 1) ||
-                (board[newHeadPositionY, newHeadPositionX] == 1))
+                (board[newHeadPositionY, newHeadPositionX] == CellType.body))
             {
                 timer1.Enabled = false;
                 MessageBox.Show("You lost! =( Your Score: " + (SnakeLength - 1));
@@ -176,45 +159,43 @@ namespace Snake
                 return;
             }
 
-            if ((indexX[0] + offsetX == foodIndexX) && (indexY[0] + offsetY == foodIndexY))
+            if ((SnakeSegments[0].X + offsetX == foodIndexX) && (SnakeSegments[0].Y + offsetY == foodIndexY))
             {
                 SnakeLength++;
-                timer1.Interval -= 10;
+                timer1.Interval -= 5;
                 for (int i = SnakeLength - 1; i > 0; i--)
                 {
-                    indexX[i] = indexX[i - 1];
-                    indexY[i] = indexY[i - 1];
+                    SnakeSegments[i].X = SnakeSegments[i - 1].X;
+                    SnakeSegments[i].Y = SnakeSegments[i - 1].Y;
                 }
-                indexX[0] = foodIndexX;
-                indexY[0] = foodIndexY;
-                board[foodIndexY, foodIndexX] = 1;
+                SnakeSegments[0].X = foodIndexX;
+                SnakeSegments[0].Y = foodIndexY;
+                board[foodIndexY, foodIndexX] = CellType.body;
                 do
                 {
                     foodIndexX = random.Next(0, N);
                     foodIndexY = random.Next(0, N);
                 }
-                while (board[foodIndexY, foodIndexX] == 1);
+                while (board[foodIndexY, foodIndexX] == CellType.body);
 
-                board[foodIndexY, foodIndexX] = 2;
+                board[foodIndexY, foodIndexX] = CellType.food;
                 pictureBox1.Refresh();
             }
             else
             {
-                board[indexY[SnakeLength - 1], indexX[SnakeLength - 1]] = 0;
+                board[SnakeSegments[SnakeLength - 1].Y, SnakeSegments[SnakeLength - 1].X] = 0;
                 for (int i = SnakeLength - 1; i > 0; i--)
                 {
-                    indexX[i] = indexX[i - 1];
-                    indexY[i] = indexY[i - 1];
+                    SnakeSegments[i].X = SnakeSegments[i - 1].X;
+                    SnakeSegments[i].Y = SnakeSegments[i - 1].Y;
                 }
                 
-                indexY[0] += offsetY;
-                indexX[0] += offsetX;
-                board[indexY[0], indexX[0]] = 1;
-                board[indexY[SnakeLength - 1], indexX[SnakeLength - 1]] = 0;
-                //indexX[SnakeLength - 1] = -1;
-                //indexY[SnakeLength - 1] = -1;
+                SnakeSegments[0].Y += offsetY;
+                SnakeSegments[0].X += offsetX;
+                board[SnakeSegments[0].Y, SnakeSegments[0].X] = CellType.body;
+                board[SnakeSegments[SnakeLength - 1].Y, SnakeSegments[SnakeLength - 1].X] = 0;
                 for (int i = 0; i < SnakeLength; i++)
-                    board[indexY[i], indexX[i]] = 1;
+                    board[SnakeSegments[i].Y, SnakeSegments[i].X] = CellType.body;
                 pictureBox1.Refresh();
             }
         }
